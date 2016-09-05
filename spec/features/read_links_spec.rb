aa5033d305c95eb7f13d35c07f16feff3f87d468
Ewan Sheldon <ewansheldon@gmail.com>
2016-09-05 16:17:54 +0100

    link class setup with database

---
+++ spec/features/read_links_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'show links' do
+  scenario 'list of added links available' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content 'Makers Academy'
+    end
+  end
+end
